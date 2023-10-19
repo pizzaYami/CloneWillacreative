6f27cd63f103a961b37f45703881f104eb44d348
pizzaYami <cyj9407@naver.com>
2023-10-19 12:40:40 +0900

    💡 Init: react router 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './Page/Home';
 
 function App() {
-  return <div>a</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<div>어바웃페이지임</div>} />
+    </Routes>
+  );
 }
 
 export default App;
