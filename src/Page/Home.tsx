130a9a8081a18ae03369725bcd580d6e080053e7
pizzaYami <cyj9407@naver.com>
2023-10-20 09:59:03 +0900

    ⚠ Fix: leftVideo 토큰포함된 링크 변경

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -8,7 +8,7 @@ const leftScreenData = [
 ];
 
 const leftVideo =
-  'https://download-video.akamaized.net/v2-1/playback/d3b3d719-f04b-466d-b847-ab1861100de4/38abdc0b-0eab056b?__token__=st=1697707848~exp=1697722248~acl=%2Fv2-1%2Fplayback%2Fd3b3d719-f04b-466d-b847-ab1861100de4%2F38abdc0b-0eab056b%2A~hmac=bac0f197d16abf4540078825a3c77b016d99f06e1a54bad0f12b99ff7ff35446&r=dXMtY2VudHJhbDE%3D';
+  'https://player.vimeo.com/progressive_redirect/playback/790130112/rendition/1080p/file.mp4?loc=external&signature=5c93f76c51d11bfb23a3fcebb111838fc29683c664715e4008dab67b967aa2dd';
 
 const rightScreenData = [
   'https://willacreative.com/wp-content/uploads/home2-1-819x1024.jpg',
@@ -37,7 +37,6 @@ const Home = () => {
   };
 
   const handleMouseMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    event.stopPropagation();
     setMousePosition({
       x: event.clientX,
       y: event.clientY,
@@ -49,7 +48,7 @@ const Home = () => {
       <ScreenContainerS>
         <LeftScreenS onMouseEnter={leftPlusNum} leftNum={leftNum}>
           <img src={leftScreenData[leftNum % 4]} alt='leftscreen' />
-          <video src={rightVideo} muted loop autoPlay />
+          <video src={leftVideo} muted loop autoPlay />
         </LeftScreenS>
         {/* <MiddleScreenS></MiddleScreenS> */}
         <RightScreenS onMouseEnter={rightPlusNum} rightNum={rightNum}>
@@ -71,7 +70,6 @@ const ContainerS = styled.main`
 `;
 
 const ScreenContainerS = styled.section`
-  position: relative;
   width: 100%;
   height: 100%;
   object-fit: cover;
