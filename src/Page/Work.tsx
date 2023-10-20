69828e4bef3cb92158010a9f1e40b42402d7829a
pizzaYami <cyj9407@naver.com>
2023-10-20 13:54:11 +0900

    ✨ Feat: navbar 색변경 이미지

--- src/Page/Work.tsx
+++ src/Page/Work.tsx
@@ -12,6 +12,10 @@ const Work = () => {
           <li>content</li>
           <li>branding</li>
         </ul>
+        <img
+          src='https://willacreative.com/wp-content/themes/twentytwelve/images/circle-animation.png'
+          alt='colorCircle'
+        />
       </NavBarS>
     </WorkContainer>
   );
@@ -20,12 +24,16 @@ const Work = () => {
 export default Work;
 
 const WorkContainer = styled.main`
-  background-color: black;
+  width: 100dvw;
+  height: 100dvh;
 `;
 
 const NavBarS = styled.nav`
-  margin-top: 73px;
   padding: 32px 25px;
+  margin-top: 73px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   ul {
     display: flex;
     gap: 24px;
@@ -34,5 +42,24 @@ const NavBarS = styled.nav`
   li:hover {
     text-transform: uppercase;
     cursor: pointer;
+  }
+  img {
+    width: 22px;
+    height: 22px;
+    animation-name: move;
+    transition-duration: 2s;
+    animation-iteration-count: infinite;
+  }
+
+  @keyframes move {
+    /* @keyframes 뒤에 애니메이션을 대표하는 임의의 이름 move를 부여 */
+    from {
+      /* 애니메이션 시작 시점 */
+      rotate: 0;
+    }
+    to {
+      /* 애니메이션 종료 시점 */
+      rotate: 180;
+    }
   }
 `;
