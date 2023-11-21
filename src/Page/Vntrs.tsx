2399d6ba6ed16f68651e29bc21c0d9752d88ddf1
pizzaYami <cyj9407@naver.com>
2023-11-21 22:15:03 +0900

    ✨ Feat: Vntrs content 추가

--- src/Page/Vntrs.tsx
+++ src/Page/Vntrs.tsx
@@ -7,7 +7,20 @@ const Vntrs = () => {
   return (
     <Container>
       <Header />
-      <NavBar />
+      <NavBar bgBtn={false} list={['about', 'portfolio', 'contact']} />
+      <ContentWrapper>
+        <video
+          src='https://player.vimeo.com/progressive_redirect/playback/794669238/rendition/1080p/file.mp4?loc=external&signature=5274a5b50d2853953af90c3a4d60b32325f27b4007a4d670d35160068bc88d47'
+          muted
+          loop
+          autoPlay
+        />
+        <div>
+          investing in and incubating
+          <br /> e-commerce products for good.
+        </div>
+      </ContentWrapper>
+
       <Footer />
     </Container>
   );
@@ -15,4 +28,48 @@ const Vntrs = () => {
 
 export default Vntrs;
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const ContentWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  video {
+    height: 650px;
+    width: 100%;
+    object-fit: cover;
+  }
+  div {
+    text-transform: uppercase;
+    font-size: 74px;
+    line-height: 85px;
+    margin: 38px 0 0px;
+    padding: 0 32px;
+    letter-spacing: -2.96px;
+  }
+  @media screen and (max-width: 767px) {
+    video {
+      height: 240px;
+    }
+    div {
+      font-size: 32px;
+      line-height: 41px;
+      letter-spacing: -1.28px;
+      margin-top: 50px;
+    }
+  }
+  @media screen and (min-width: 768px) and (max-width: 1198px) {
+    div {
+      font-size: 42px;
+      line-height: 52px;
+    }
+  }
+
+  @media screen and (min-width: 1199px) {
+    div {
+      font-size: 74px;
+    }
+  }
+`;
