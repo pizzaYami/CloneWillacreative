eba43fa29ef3fa5a2b05f05e4d5ac6eebdf75258
pizzaYami <cyj9407@naver.com>
2024-01-05 18:29:25 +0900

    🎨 Style: css reset 진행

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -44,66 +44,45 @@ const GlobalStyle = createGlobalStyle`
     }
   }
   
-  
-
-  *{
-    margin: 0;
-    border: 0;
-    box-sizing: border-box;
-  }
-
-  h1, h2, h3, p, li, div{
-    white-space: pre-line;
-    font-weight: 500;
-  }
-
-  html, body{
-    width:100%;
-    height:100vh;
-  }
-  
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
+  * {
+  margin: 0; /* margin 초기화 */
+  padding: 0; /* padding 초기화 */
+  font: inherit; /* font 상속을 못하는 요소(button, input)에게 폰트 적용을 위해 작성 */
+  color: inherit; /* a, input, textarea에도 동일한 글자색 적용을 위해 작성 */
 }
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-	display: block;
+
+*,
+:after,
+:before {
+  box-sizing: border-box;
 }
+
+:root {
+  -webkit-text-size-adjust: 100%; /* 모바일에서도 원래 폰트 사이즈로 출력(사파리) */
+  text-size-adjust: 100%; /* 모바일에서도 원래 폰트 사이즈로 출력 */
+  overflow-wrap: break-word;
+}
+
+html,
 body {
-	line-height: 1;
+  height: 100%;
 }
-ol, ul {
-	list-style: none;
+
+img,
+svg,
+canvas {
+  display: block; /* 하단의 여백 제거를 위해 작성 */
+  max-width: 100%; /* 이미지 삐져나가는것 방지하기 위해 작성 */
 }
-blockquote, q {
-	quotes: none;
+
+button {
+  background: none;
+  border: 0;
+  cursor: pointer;
 }
-blockquote:before, blockquote:after,
-q:before, q:after {
-	content: '';
-	content: none;
-}
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
+
+a {
+  text-decoration: none;
 }
 `;
 
