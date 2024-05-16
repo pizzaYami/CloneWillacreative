93aad37c5bf0b522c900c451eaf6c05738fe80ff
pizzaYami <cyj9407@naver.com>
2024-05-16 22:20:08 +0900

    Refactor: home 데이터 Constant 추가

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,24 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
-
-const leftScreenData = [
-  'https://willacreative.com/wp-content/uploads/Rectangle-3841-2-819x1024.png',
-  'https://willacreative.com/wp-content/uploads/Rectangle-11-4.png',
-  'https://willacreative.com/wp-content/uploads/home2h.jpg',
-];
-
-const leftVideo =
-  'https://player.vimeo.com/progressive_redirect/playback/790130112/rendition/1080p/file.mp4?loc=external&signature=5c93f76c51d11bfb23a3fcebb111838fc29683c664715e4008dab67b967aa2dd';
-
-const rightScreenData = [
-  'https://willacreative.com/wp-content/uploads/home2-1-819x1024.jpg',
-  'https://willacreative.com/wp-content/uploads/home3-819x1024.jpg',
-  'https://willacreative.com/wp-content/uploads/Screen-Shot-2021-07-28-at-9.23-1-819x1024.png',
-];
-
-const rightVideo =
-  'https://player.vimeo.com/progressive_redirect/playback/790304859/rendition/1080p/file.mp4?loc=external&signature=5fcf83fa08820463173a638eff04c5028ac14b09fad588c0aa0f58586403ff6d';
+import { leftScreenData, leftVideo, rightScreenData, rightVideo } from '../constants/homeConstant';
 
 const Home = () => {
   const navigate = useNavigate();
