6735bd166550ac1aaf913a5645b1f77bb19a0010
pizzaYami <cyj9407@naver.com>
2024-05-24 16:39:16 +0900

    Refactor: workDoSection 리팩토링

--- src/Component/Work/WorkDoSection.tsx
+++ src/Component/Work/WorkDoSection.tsx
@@ -1,44 +1,29 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { useEffect, useState } from 'react';
-import { styled } from 'styled-components';
-
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+import { workDoSectionData } from "../../constants/workConstant";
 const WorkDoSection = () => {
   const [isLazy, setIsLazy] = useState(0);
+
   useEffect(() => {
-    setTimeout(() => {
-      setIsLazy(1000);
-    }, 1000);
-    setTimeout(() => {
-      setIsLazy(1500);
-    }, 1500);
-    setTimeout(() => {
-      setIsLazy(2000);
-    }, 2000);
+    const timeouts = [1000, 1500, 2000];
+    timeouts.forEach((time) => {
+      setTimeout(() => {
+        setIsLazy(time);
+      }, time);
+    });
   }, []);
 
   return (
     <WorkDoSectionS>
-      <span>what we do</span>
-      <ul>
-        <li>
-          <a className={isLazy >= 1000 ? 'active-top active' : ''}>gtm strategy</a>
-        </li>
-        <li>
-          <a className={isLazy >= 1000 ? 'active-top active' : ''}>web design</a>
-        </li>
-        <li>
-          <a className={isLazy >= 1500 ? 'active' : ''}>branding & packaging</a>
-        </li>
-        <li>
-          <a className={isLazy >= 1500 ? 'active' : ''}>web development</a>
-        </li>
-        <li>
-          <a className={isLazy >= 2000 ? 'active' : ''}>content production</a>
-        </li>
-        <li>
-          <a className={isLazy >= 2000 ? 'active' : ''}>web 3.0 & 3d design</a>
-        </li>
-      </ul>
+      <h5>what we do</h5>
+      <WorkDoSectionListS>
+        {workDoSectionData.map((item) => (
+          <li key={item.text}>
+            <a className={isLazy >= item.time ? "active" : ""}>{item.text}</a>
+          </li>
+        ))}
+      </WorkDoSectionListS>
     </WorkDoSectionS>
   );
 };
@@ -46,34 +31,32 @@ const WorkDoSection = () => {
 export default WorkDoSection;
 
 const WorkDoSectionS = styled.div`
-  margin: 15px 0 87px 0;
+  margin: 15px 0 90px 0;
   width: 100%;
-  span {
+  h5 {
     padding: 0 19px 14px;
     font-size: 14px;
     display: block;
   }
-  ul {
-    display: flex;
-    flex-wrap: wrap;
-    width: 100%;
-  }
+`;
+
+const WorkDoSectionListS = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
   li {
-    width: 50%;
     padding: 0 19px;
-    font-size: 32px;
-    font-weight: normal;
-    line-height: normal;
     text-transform: uppercase;
-    text-decoration: none;
-    letter-spacing: -0.64px;
     @media screen and (max-width: 767px) {
+      width: 100%;
       font-size: 18px;
     }
     @media screen and (min-width: 768px) and (max-width: 1198px) {
+      width: 50%;
       font-size: 28px;
     }
     @media screen and (min-width: 1199px) {
+      width: 50%;
       font-size: 32px;
     }
   }
@@ -85,7 +68,7 @@ const WorkDoSectionS = styled.div`
     transform: translate(0, 30px);
     opacity: 0;
     &::before {
-      content: '';
+      content: "";
       position: absolute;
       top: 0;
       left: 0;
@@ -95,7 +78,7 @@ const WorkDoSectionS = styled.div`
       opacity: 0.5;
     }
     &::after {
-      content: '';
+      content: "";
       position: absolute;
       bottom: 0;
       left: 0;
@@ -113,15 +96,5 @@ const WorkDoSectionS = styled.div`
         transition: width 0.9s ease-in;
       }
     }
-    &.active-top {
-      &::before {
-        width: 100%;
-        transition: width 0.9s ease-in;
-      }
-    }
-  }
-
-  .no-border-top {
-    border-top: 0px solid black;
   }
 `;
